6ed3f59c32ffbad69420bbe1b698282f92a7d260
Gavin Reeves <gavinandrewreeves@gmail.com>
2019-10-03 21:28:33 +0200

    Updated to show chosen bucket contents before and after

--- S3-bucket-upload-script.py
+++ S3-bucket-upload-script.py
@@ -33,10 +33,18 @@ def main_func():
     buckets = get_list_of_buckets()
     users_input = raw_input("Please input the number of the bucket you would like to upload your file into:  ")
     users_bucket = get_user_input(users_input, buckets)
+    my_bucket = s3.Bucket('gavswebsite')
+    bucket_contents = []
     print("You have selected the following bucket to upload file(s) to:  " + users_bucket)
 
     print("###########################")
-    user_choice = raw_input("Do you want to go ahead and upload a file to your chossen S3 bucket?:   ")
+
+    print("Here is the contents of your chosen bucket")
+    for object_summary in my_bucket.objects.filter():
+        print(object_summary.key)
+
+
+    user_choice = raw_input("Do you want to go ahead and upload a file to your chosen S3 bucket?:   ")
     if user_choice == "yes":
         users_file_path = raw_input("Please input the path of the file that you want to upload e.g. /home/ec2-user/mew/    ")
         users_file = raw_input("Please input the name of the file that you wish to upload:  ")
@@ -44,6 +52,12 @@ def main_func():
         print("You have chosen to upload the following file:   ")
         print(file_and_path)
         s3.meta.client.upload_file(file_and_path, users_bucket, users_file,ExtraArgs={'ACL':'public-read'})
+
+        print("now your bucket contents is as follows:")
+        print("--------------------------------------")
+        for object_summary in my_bucket.objects.filter():
+            print(object_summary.key)
+
     else:
         print("You have chosen not to upload a file")
 
