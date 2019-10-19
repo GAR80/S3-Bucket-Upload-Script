0582320c51e086897e4c6f3ef532f9371a65b237
Gavin Reeves <gavinandrewreeves@gmail.com>
2019-10-19 12:12:49 +0200

    Added option for user to continue adding and removing files from the same bucket or a different bucket

--- S3-bucket-upload-script.py
+++ S3-bucket-upload-script.py
@@ -34,40 +34,56 @@ def get_user_input(user_input, buckets):
     return bucketchoice
 
 def main_func():
-    print_all_buckets()
-    buckets = get_list_of_buckets()
-    users_input = raw_input("Please input the number of the bucket you would like to upload or delete a file from:  ")
-    users_bucket = get_user_input(users_input, buckets)
-    my_bucket = s3.Bucket(users_bucket)
-    bucket_contents = []
-    print("You have selected the following bucket:  " + users_bucket)
+    program_running = True
+    choose_bucket = True
+    while program_running == True:
+        while choose_bucket == True:
+            print_all_buckets()
+            buckets = get_list_of_buckets()
+            users_input = raw_input("Please input the number of the bucket you would like to upload or delete a file from:  ")
+            users_bucket = get_user_input(users_input, buckets)
+            my_bucket = s3.Bucket(users_bucket)
+            bucket_contents = []
+            print("You have selected the following bucket:  " + users_bucket)
 
-    print("###########################")
+            print("###########################")
 
-    print("Here is the contents of your chosen bucket")
-    list_bucket_contents(my_bucket)
+            print("Here is the contents of your chosen bucket")
+            list_bucket_contents(my_bucket)
+            choose_bucket = False
 
-    user_choice = raw_input("Do you want to upload a file or delete a file from your chosen S3 bucket? please select upload or delete:   ")
-    if user_choice == "upload":
-        users_file_path = raw_input("Please input the path of the file that you want to upload e.g. /home/ec2-user/mew/    ")
-        users_file = raw_input("Please input the name of the file that you wish to upload:  ")
-        file_and_path = str(users_file_path) + "/" + str(users_file)
-        print("You have chosen to upload the following file:   ")
-        print(file_and_path)
-        s3.meta.client.upload_file(file_and_path, users_bucket, users_file,ExtraArgs={'ACL':'public-read'})
+        user_choice = raw_input("Do you want to upload a file or delete a file from your chosen S3 bucket? please select upload or delete:   ")
+        if user_choice == "upload":
+            users_file_path = raw_input("Please input the path of the file that you want to upload e.g. /home/ec2-user/mew/    ")
+            users_file = raw_input("Please input the name of the file that you wish to upload:  ")
+            file_and_path = str(users_file_path) + "/" + str(users_file)
+            print("You have chosen to upload the following file:   ")
+            print(file_and_path)
+            s3.meta.client.upload_file(file_and_path, users_bucket, users_file,ExtraArgs={'ACL':'public-read'})
 
-        print("now your bucket contents is as follows:")
-        print("--------------------------------------")
-        list_bucket_contents(my_bucket)
-    elif user_choice == "delete":
-        print("You have chosen to delete a file from your bucket:  " + str(users_bucket))
-        file_to_remove = raw_input("type the exact name of the file that you wish to delete for example gates.txt: ")
-        s3.Object(users_bucket, file_to_remove).delete()
-        print("Now your bucket looks as follows:  ")
-        list_bucket_contents(my_bucket)
+            print("now your bucket contents is as follows:")
+            print("--------------------------------------")
+            list_bucket_contents(my_bucket)
+        elif user_choice == "delete":
+            print("You have chosen to delete a file from your bucket:  " + str(users_bucket))
+            file_to_remove = raw_input("type the exact name of the file that you wish to delete for example gates.txt: ")
+            s3.Object(users_bucket, file_to_remove).delete()
+            print("Now your bucket looks as follows:  ")
+            list_bucket_contents(my_bucket)
 
-    else:
-        print("You have chosen not to upload a file")
+        else:
+            print("You have chosen not to upload or delete a file")
+
+        user_continue = raw_input("Would you like another transaction? (yes/no) ")
+        if user_continue == "yes":
+            program_running = True
+            use_same_bucket = raw_input("Do you want to use the same bucket? (yes/no) ")
+            if use_same_bucket == "yes":
+                choose_bucket = False
+            else:
+                choose_bucket = True
+        elif user_continue == "no":
+            program_running = False
 
     return ""
 
